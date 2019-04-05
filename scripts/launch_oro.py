3bfe1db3e56cfd47f707dbcfe41a14f37a7b8f6b
Yoan Sallami <ysallami@laas.fr>
2019-04-05 13:25:24 +0200

    :ambulance: fix bug 	modifié :         launch/knowledge_base.launch 	modifié :         scripts/launch_oro.py 	modifié :         scripts/uwds_knowledge_base.py

--- scripts/launch_oro.py
+++ scripts/launch_oro.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     rospy.init_node("oro-launcher", anonymous=False)
     verbose = rospy.get_param("~verbose", True)
     robot_pkg_base = rospy.get_param("~robot_pkg_base", "~/openrobots")
-    cmd = "cd "+robot_pkg_base+" && oro-server" if verbose == True else "cd "+robot_pkg_base+" && oro-server > null"
+    cmd = "cd "+robot_pkg_base+" && oro-server" if verbose is True else "cd "+robot_pkg_base+" && oro-server > null"
     os.system(cmd)
--- scripts/uwds_knowledge_base.py
+++ scripts/uwds_knowledge_base.py
@@ -4,66 +4,104 @@
 import rospy
 from uwds_msgs.srv import QueryInContext
 from pyuwds.uwds_client import UwdsClient
-from pyuwds.uwds import UNDEFINED
+from pyuwds.uwds import READER
 from pyoro import Oro
 
 class KnowledgeBase(UwdsClient):
     def __init__(self):
-        UwdsClient.__init__("uwds_knowledge_base", UNDEFINED)
+        UwdsClient.__init__(self, "uwds_knowledge_base", READER)
         hostname = rospy.get_param("~oro_host", "localhost")
         port = rospy.get_param("~oro_port", "6969")
-        self.kb = Oro(hostname, port)
+        success = False
+        while not success and not rospy.is_shutdown():
+            try:
+                self.kb = Oro(hostname, int(port))
+                success = True
+            except Exception as e:
+                pass
+        rospy.loginfo("Connected to the Oro knowledge base")
         self.query_service = rospy.Service("uwds/query_knowledge_base", QueryInContext, self.handleQuery)
+        rospy.loginfo("Underworlds KB ready !")
 
     def addNode(self, world_name, node):
         namespace = world_name.split("/")
         agent = namespace[0]
         world = namespace[1]
 
-        scene = ctx.worlds[world_name].scene
+        scene = self.ctx.worlds[world_name].scene
 
         types_str = scene.getNodeProperty(node.id, "class")
-        if types_str != "" :
+        if types_str != "":
             types = types_str.split(",")
-        else :
-            if node.type == MESH : types.append("TangibleThing")
-            if node.type == ENTITY : type.append("LocalizedThing")
-            if node.type == CAMERA : type.append("ExistingThing")
+        else:
+            if node.type == MESH: types.append("TangibleThing")
+            if node.type == ENTITY: type.append("LocalizedThing")
+            if node.type == CAMERA: type.append("ExistingThing")
 
         for type in types:
             if agent == "robot":
                 self.kb.add(node.id+" rdf:Type "+property.data, world)
-            else :
+            else:
                 self.kb.addForAgent(agent, node.id+" rdf:Type "+property.data, world)
         if agent == "robot":
             success = self.kb.checkConsistency()
-        else :
+        else:
             success = self.kb.checkForAgent(agent)
         return success
 
     def removeNode(self, world_name, node_id):
+        """
+        """
         pass
 
     def addSituation(self, world_name, situation):
-        pass
-
-    def removeSituation(self, world_name, situation):
-        pass
-
-    def queryKnowledgeBase(self, world_name, query):
+        """
+        """
         namespace = world_name.split("/")
         agent = namespace[0]
         world = namespace[1]
-        if agent == "robot" :
-            result = self.kb.find(world, query.split(","))
-        else:
-            result = self.kb.findForAgent(agent, world, query.split(","))
         pass
 
-    def handleQuery(req):
-        result = self.queryKnowledgeBase(req.ctxt.world, req.query)
-        return result, True, ""
+    def removeSituation(self, world_name, situation):
+        """
+        """
+        pass
+
+    def queryKnowledgeBase(self, world_name, query):
+        """
+        """
+        namespace = world_name.split("/")
+        agent = namespace[0]
+        world = namespace[1]
+        result = []
+        if(self.verbose):
+            rospy.loginfo("Query the <"+world_name+"> world : "+query)
+
+            if agent == "robot":
+                result = self.kb.find(world, query.split(","))
+            else:
+                result = self.kb.findForAgent(agent, world, query.split(","))
+
+        result_final = []
+        for element in result:
+            if self.ctx.worlds[world_name].scene.nodes.has(element):
+                result_final.append(element)
+            elif self.ctx.worlds[world_name].timeline.situations.has(element):
+                result_final.append(element)
+        return result_final
+
+    def handleQuery(self, req):
+        """
+        """
+        try:
+            result = self.queryKnowledgeBase(req.ctxt.world, req.query)
+            return result, True, ""
+        except Exception as e:
+            rospy.logwarn("[uwds::queryKnowledgeBase] Exception occurred : "+str(e))
+            return [], False, str(e)
+
 
 if __name__ == '__main__':
+    rospy.init_node("uwds_knowledge_base", anonymous=False)
     kb = KnowledgeBase()
     rospy.spin()
